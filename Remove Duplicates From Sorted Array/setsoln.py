d1e7fcf0b847fd382675fd4c952ae576161d4bfd
Chackochan Sebastian <95061303+Chackoseb@users.noreply.github.com>
2023-07-15 20:40:24 +0530

    Update setsoln.py

--- Remove Duplicates From Sorted Array/setsoln.py
+++ Remove Duplicates From Sorted Array/setsoln.py
@@ -7,3 +7,12 @@ class Solution(object):
         k=len(nums)
         nums[:]=sorted(set(nums))
         return len(nums)
+        
+ '''Common Wrong Answers:
+	nums = sorted(set(nums))
+	return len(nums)
+ nums =  doesn't replace elements in the original list.
+nums[:] = replaces element in place
+
+In short, without [:], we're creating a new list object, which is against what this problem is asking for:
+Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.'''
