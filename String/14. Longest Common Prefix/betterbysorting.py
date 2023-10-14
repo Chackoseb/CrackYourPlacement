590772092e62fdb6251676b7c9a7a5296a3463e0
Chackochan Sebastian <95061303+Chackoseb@users.noreply.github.com>
2023-10-14 16:49:48 +0530

    Create betterbysorting.py

---
+++ String/14. Longest Common Prefix/betterbysorting.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        strs.sort()
+        s1 = strs[0]
+        s2 = strs[len(strs)-1]
+        i = 0
+        while (i<len(s1) and i<len(s2)):
+            if s1[i] == s2[i]:
+                i += 1
+            else:
+                break
+        return s1[0:i]
