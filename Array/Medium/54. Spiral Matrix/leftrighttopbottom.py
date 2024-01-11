e99365b8a868e47d6d68e6e1821f3d64319ecc49
Chackochan Sebastian <95061303+Chackoseb@users.noreply.github.com>
2024-01-11 20:39:17 +0530

    Create leftrighttopbottom.py

---
+++ Array/Medium/54. Spiral Matrix/leftrighttopbottom.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def spiralOrder(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: List[int]
+        """
+        res = []
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+
+        while left < right and top < bottom:
+            #traverse every i in top row
+            for i in range(left, right):
+                res.append(matrix[top][i])
+            top += 1
+
+            #traverse every i in right col
+            for i in range(top, bottom):
+                res.append(matrix[i][right-1])
+            right -= 1
+
+            #other type of matrix where rowsize > colsize check
+            if not (left < right and top < bottom):
+                break
+
+            #i in bottom row
+            for i in range(right-1, left-1, -1):
+                res.append(matrix[bottom-1][i])
+            bottom -= 1
+
+            #every i in left col
+            for i in range(bottom-1, top-1, -1):
+                res.append(matrix[i][left])
+            left += 1
+
+        return res
